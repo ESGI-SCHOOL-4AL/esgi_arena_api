627de494015d337ee07e96563d8d77302b38854a
eu777 <enes.uyar@pm.me>
2020-05-29 02:23:53 +0200

    initial commit

---
+++ src/main/java/com/esgi/projetannuel/ProjetannuelApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.projetannuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetannuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetannuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esgi/projetannuel/ProjetannuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.projetannuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetannuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
